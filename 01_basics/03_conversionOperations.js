af4eea0a5572a0dadc180eb74be0fe1219387d7f
YuganshKhatri <yuganshkhatri67@gmail.com>
2026-01-07 09:26:47 +0000

    conversions

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+// let score="33abc"
+// const ValueInNumber=Number(score)
+// console.log(typeof score);
+// console.log(typeof ValueInNumber)
+// console.log(ValueInNumber);
+// let LoggedIn="Yugansh"
+// let booleanLoggedIn=Boolean(LoggedIn)
+// console.log(booleanLoggedIn)
+let number=33
+let Stringnumber=String(number)
+console.log(typeof Stringnumber);
+console.log(Stringnumber);
+
+
+
+
+
