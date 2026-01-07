05c9487a60bc4cb3675d3b6ca86af5e95ce9c62d
YuganshKhatri <yuganshkhatri67@gmail.com>
2026-01-07 08:54:21 +0000

    changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=7777
+let accountEmail="Yugansh@google.com"
+var accountPass="Yugansh"
+accountCity="Delhi"
+let accountState
+
+//accountId=5
+accountEmail="Yugansh@gggg.com"
+accountPass="0706"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
+
