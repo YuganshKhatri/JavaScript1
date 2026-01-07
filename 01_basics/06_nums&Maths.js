ad8e2ea2c8b9435e9e3536f8ed12882adcdd4251
YuganshKhatri <yuganshkhatri67@gmail.com>
2026-01-07 13:09:41 +0000

    Number & Maths

--- 01_basics/06_nums&Maths.js
+++ 01_basics/06_nums&Maths.js
@@ -1,1 +1,21 @@
-console.log("Yugansh")
+// const score=400
+// const balance=new Number(100.458999999)
+// console.log(score)
+// console.log(balance)
+// console.log(balance.toFixed(3))
+// const StringBalance=balance.toString()
+// console.log(StringBalance)
+// console.log(StringBalance.length)
+// const price=123.5966
+// console.log(price.toPrecision(4))
+// const hund=10000000
+// console.log(hund.toLocaleString('en-IN'))
+
+//                              MATHS
+console.log(Math) // Math library is an object (Math -> object)
+console.log(Math.ceil(2.890999))
+console.log(Math.round(4.8))
+console.log(Math.random())
+const min=1
+const max=6
+console.log(Math.floor(Math.random()*(max-min+1))+min)
