954b3eb0ae907bb030309076a37ddb189f92b4b3
YuganshKhatri <yuganshkhatri67@gmail.com>
2026-01-07 12:15:22 +0000

    learn

--- 01_basics/05_StringsBasics.js
+++ 01_basics/05_StringsBasics.js
@@ -3,12 +3,13 @@ let lname="khatri"
 let repocount=18
 console.log(`Hello my name is ${name+" "+lname} and my repo count is ${repocount}`) //String Interpolation
 const gamename="   Mario  "
-const gamename1=new String('Mario')
+const gamename1=new String('   Mario   ')
 console.log(gamename)
 console.log(gamename1)
-console.log(gamename1.endsWith('o'))
+console.log(gamename1.endsWith(' '))
 console.log(gamename1.length)
 console.log(gamename1.toUpperCase())
 console.log(gamename1.toLowerCase())
 console.log(gamename1.substring(0,4))
 console.log(gamename1.trim())
+console.log(gamename.endsWith(' '))
