a6bdc1bff23362850b0aef1f55388e69d41ba553
YuganshKhatri <yuganshkhatri67@gmail.com>
2026-01-07 11:25:49 +0000

    summary on datatypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -27,6 +27,19 @@ let y=x++
 let z=++x
 console.log(y)
 console.table([y, z])
-
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id)
+console.log(anotherId)
+console.log(id===anotherId)
+const heroes=["Ironman", "Captain America", "Hulk", "Thor"]
+const heroes1=["Ironman", "Captain America", "Hulk"]
+console.log(heroes)
+console.table([heroes])
+console.table([heroes1])
+const Myfunction=function(){
+    console.log("Hello World")
+}
+Myfunction()
 
 
